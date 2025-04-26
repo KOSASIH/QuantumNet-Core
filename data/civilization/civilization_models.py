a33579faa5c4ee0c07420a3f736c11d3c7bf57e5
KOSASIH <kosasihg88@gmail.com>
2025-04-26 15:02:13 +0700

    Create civilization_models.py

---
+++ data/civilization/civilization_models.py
@@ -0,0 +1,27 @@
+[
+    {
+        "id": 0,
+        "population": 12345,
+        "technology_level": 0.67,
+        "resources": {
+            "food": 5000,
+            "water": 3000,
+            "energy": 7000
+        },
+        "happiness_index": 0.85,
+        "civilization_name": "Civilization 0"
+    },
+    {
+        "id": 1,
+        "population": 67890,
+        "technology_level": 0.45,
+        "resources": {
+            "food": 8000,
+            "water": 6000,
+            "energy": 9000
+        },
+        "happiness_index": 0.75,
+        "civilization_name": "Civilization 1"
+    }
+    // More models...
+]
